249c60bc3481ffa557f45965c1340bf40e0e59c2
Sreenath T S <sreenathts655@gmail.com>
2025-12-11 03:49:41 +0530

    Add files via upload

---
+++ program/second_largest.py
@@ -0,0 +1,13 @@
+def second_largest(arr):
+    largest=float('-inf')
+    second=float('-inf')
+    for i in arr:
+        if i>largest:
+            
+            second=largest
+            largest=i
+        elif i>second and i!=largest:
+            second=i
+    return second
+arr=[1,2,3,4,5]
+print(second_largest(arr))            
